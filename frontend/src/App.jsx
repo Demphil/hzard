6f1095c25fee6a3f395dc2e0a47f018d294ed5d5
Demphil <92610592+Demphil@users.noreply.github.com>
2025-05-14 01:24:36 +0100

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VideoProcessor from './components/VideoProcessor';
+
+function App() {
+  return (
+    <div className="app">
+      <VideoProcessor />
+    </div>
+  );
+}
+
+export default App;
