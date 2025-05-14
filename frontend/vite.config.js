dbc8439c71f7776720a58aae8d9e1105120e31c1
Demphil <92610592+Demphil@users.noreply.github.com>
2025-05-14 03:43:52 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
 export default defineConfig({
   root: path.resolve(__dirname, './'),
   publicDir: path.resolve(__dirname, './public'),
+  plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true,
     rollupOptions: {
-      input: path.resolve(__dirname, './public/index.html')
+      input: path.resolve(__dirname, 'public/index.html')
     }
   }
 })
