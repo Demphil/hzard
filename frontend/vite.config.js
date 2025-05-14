7fa6d864aa600c14f72fb86b526f45861be13b37
Demphil <92610592+Demphil@users.noreply.github.com>
2025-05-14 03:02:55 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  root: path.resolve(__dirname, './src'), // المسار إلى ملفات المصدر
-  publicDir: path.resolve(__dirname, './public'), // المسار إلى المجلد العام
-  plugins: [react()],
+  root: path.resolve(__dirname, 'public'),
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
+    outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: path.resolve(__dirname, './index.html') // المسار إلى ملف HTML الرئيسي
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src') // إنشاء alias للمسارات
+      input: path.resolve(__dirname, 'public/index.html')
     }
   }
 })
